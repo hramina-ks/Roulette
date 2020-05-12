05d8ea3cb1ae08c2d5c23c904938a0946c4d3c24
hramina-ks <krist89@yandex.ru>
2020-05-13 02:14:28 +0500

    добавила рулетку для чисел

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,12 +6,12 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import jdk.internal.org.objectweb.asm.Handle;
 
-import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class Controller {
     @FXML
@@ -26,11 +26,18 @@ public class Controller {
     @FXML
     private GridPane result; //блок результатов
 
+    @FXML
+    private TextField counter; //поле для счета игрока
+
     public class Bet {
         private CheckBox bet;
 
-        public void forBet(CheckBox[] group, int finalI, AnchorPane resultBlock) {
-            for (CheckBox j : group) {
+        public void forBet(CheckBox[] group,  AnchorPane resultBlock) {
+            for (int i = 0; i < group.length; i++) {
+                int finalI = i;
+                group[i].selectedProperty().addListener(new ChangeListener<Boolean>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {for (CheckBox j : group) {
                 if (j.equals(bet)) {
                     j.setSelected(false);
                 }
@@ -51,6 +58,9 @@ public class Controller {
                 label_num.setStyle("-fx-text-fill: gray");
                 label_win.setStyle("-fx-text-fill: gray");
             }
+                    }
+                });
+            }
         }
 
         public CheckBox startBet(CheckBox[] group) {
@@ -67,7 +77,7 @@ public class Controller {
         public CheckBox numberBet = null;
         public CheckBox colorBet = null;
         public CheckBox evenBet = null;
-
+        public int score = 10; //это количество очков у пользователя
 
     public void initialize() {
         CheckBox[] numbersGroup = new CheckBox[numbers.getChildren().size()]; //массив по количеству чекбоксов с цифрами
@@ -90,23 +100,64 @@ public class Controller {
         AnchorPane resultCol = (AnchorPane) resultBlocks.get(2);
         AnchorPane resultEven = (AnchorPane) resultBlocks.get(1);
 
-        Bet Bet = new Bet();
+        counter.setText("Всего очков " + score); //пишем очки в поле счетчика
 
-        for (int i = 0; i < numbersGroup.length; i++) {
-            int finalI = i;
-            numbersGroup[i].selectedProperty().addListener(new ChangeListener<Boolean>() {
-                @Override
-                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                    Bet.forBet(numbersGroup, finalI,resultNum);
-                }
-            });
-        }
+        Bet BetNum = new Bet();
+        BetNum.forBet(numbersGroup, resultNum);
+        Bet BetCol = new Bet();
+        BetCol.forBet(colorsGroup,resultCol);
+        Bet BetEven = new Bet();
+        BetEven.forBet(evenGroup,resultEven);
 
         btn_start.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                numberBet = Bet.startBet(numbersGroup);
+                numberBet = BetNum.startBet(numbersGroup);
+                colorBet = BetCol.startBet(colorsGroup);
+                evenBet = BetEven.startBet(evenGroup);
+                String colorBetString;
+                String evenBetString;
+                int [] black = new int[] {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
+                int [] red = new int[] {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
+                boolean isBlack = false;
+                boolean isRed = false;
+                boolean isEven = false; //четный
+
+                if (numberBet != null) {
+                    String numberBetString = numberBet.getId(); //id чекбокса, на который сделана ставка
+                    Random r = new Random();
+                    int num = r.nextInt(37); //рандомное число, выпавшее на рулетке
+                    String numText = "num"+num; //приводим число к виду id чекбокса
+                    ObservableList<Node> resultLabels = resultNum.getChildren();
+                    Label label_num = (Label)resultLabels.get(0); //это лейбл, куда пишется выпавшее число
+                    Label label_win = (Label)resultLabels.get(1); //это лейбл результата
+                    for (int i = 0; i < numbersGroup.length; i++) {
+                        int finalI = i;
+                        if (numbersGroup[finalI].getId().equals(numText)) {
+                            label_num.setText(numbersGroup[finalI].getText());
+                        }
+                    }
+                        if (numText.equals(numberBetString)) {
+                            label_win.setText("Победа");
+                            label_num.setStyle("-fx-text-fill: green;");
+                            label_win.setStyle("-fx-text-fill: green;");
+                            resultNum.setStyle("-fx-border-color: green;");
+                            score++;
+                            counter.setText("Всего очков " + score);
+                        }
+                        else {
+                            label_win.setText("Поражение");
+                            label_num.setStyle("-fx-text-fill: red;");
+                            label_win.setStyle("-fx-text-fill: red;");
+                            resultNum.setStyle("-fx-border-color: red;");
+                            score--;
+                            counter.setText("Всего очков " + score);
+                        }
+                }
+
                 System.out.println(numberBet);
+                System.out.println(colorBet);
+                System.out.println(evenBet);
             }
         });
     }
